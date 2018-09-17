ad1299511ea250019cb1cdd9f7f446781724ef17
Artem Andryukhov <mouseartiom@yandex.ru>
2018-09-17 15:32:34 +0300

    code improvements

--- source/roman.js
+++ source/roman.js
@@ -1,27 +1,30 @@
 'use strict';
 
-function fromRoman(str) {  
-    let result = 0;
-    let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    let roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
-    for (let i = 0; i <= decimal.length; i++) {
-      while (str.indexOf(roman[i]) === 0) {
-        result += decimal[i];
-        str = str.replace(roman[i], '');
-      }
-    }
-    return result;
-  }
+const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const romanum = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
 
-function toRoman(num) {  
-  if (num > 4999) {throw new TypeError('Number is too big.')}
-  if (num < 1) {throw new TypeError('Roman numerals cannot be negative.')}
+function fromRoman(str) {
+  let result = 0;
+  decimal.forEach(function(currentValue, index) {
+    while (str.indexOf(romanum[index]) === 0) {
+        result += currentValue;
+        str = str.replace(romanum[index], '');
+      }
+  });
+  return result;
+}
+
+function toRoman(num) {
+  if (num > 4999) {
+    throw new TypeError('Number is too big.')
+  }
+  if (num < 1) {
+    throw new TypeError('Roman numerals cannot be negative.')
+  }
   let result = '';
-  let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-  let roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
   for (let i = 0; i<=decimal.length; i++) {
-    while (num % decimal[i] < num) {     
-      result += roman[i];
+    while (num % decimal[i] < num) {
+      result += romanum[i];
       num -= decimal[i];
     }
   }
@@ -35,22 +38,27 @@ function isRoman(str) {
 }
 
 function roman(data) {
-  let type = typeof data;
   if (typeof data !== 'string' && typeof data !== 'number') {
     throw new TypeError('Incorrect argument.');
   }
+  let result;
   if (typeof data === 'string' ) {
     data = data.toUpperCase();
     if (!isNaN(+data)) {
-      return toRoman(data);
+      result = toRoman(data)
     }
-    if (isRoman(data)) {
-      return fromRoman(data); 
+    else if (isRoman(data)) {
+      result = fromRoman(data)
     }
-    else {throw new TypeError('Incorrect argument.')}
+    else {
+      throw new TypeError('Incorrect argument.')
+    }
   }
   else if (typeof data === 'number') {
-    if (isNaN(data)) {throw new TypeError('Incorrect argument.')}
-    return toRoman(data);
+    if (isNaN(data)) {
+      throw new TypeError('Incorrect argument.')
+    }
+    result = toRoman(data)
   }
+  return result;
 }
--- tests/roman.js
+++ tests/roman.js
@@ -1,56 +1,56 @@
 'use strict';
 
 QUnit.module('Тестируем функцию roman', function () {
-	QUnit.test('roman правильно переводит из римской системы счисления', function (assert) {
-		assert.strictEqual(roman('I'), 1);
-		assert.strictEqual(roman('V'), 5);
-		assert.strictEqual(roman('M'), 1000);
-		assert.strictEqual(roman('l'), 50);
-		assert.strictEqual(roman('d'), 500);
+  QUnit.test('roman правильно переводит из римской системы счисления', function (assert) {
+    assert.strictEqual(roman('I'), 1);
+    assert.strictEqual(roman('V'), 5);
+    assert.strictEqual(roman('M'), 1000);
+    assert.strictEqual(roman('l'), 50);
+    assert.strictEqual(roman('d'), 500);
 
-		assert.strictEqual(roman('iv'), 4);
-		assert.strictEqual(roman('iiii'), 4);
-		assert.strictEqual(roman('CM'), 900);
+    assert.strictEqual(roman('iv'), 4);
+    assert.strictEqual(roman('iiii'), 4);
+    assert.strictEqual(roman('CM'), 900);
 
-		assert.strictEqual(roman('MCMIV'), 1904);
-		assert.strictEqual(roman('MCMXC'), 1990);
-		assert.strictEqual(roman('mmxvii'), 2017);
-	});
+    assert.strictEqual(roman('MCMIV'), 1904);
+    assert.strictEqual(roman('MCMXC'), 1990);
+    assert.strictEqual(roman('mmxvii'), 2017);
+  });
 
-	QUnit.test('roman правильно переводит из десятичной системы счисления', function (assert) {
-		assert.strictEqual(roman(1), 'I');
-		assert.strictEqual(roman(5), 'V');
-		assert.strictEqual(roman(1000), 'M');
-		assert.strictEqual(roman(50), 'L');
-		assert.strictEqual(roman(500), 'D');
+  QUnit.test('roman правильно переводит из десятичной системы счисления', function (assert) {
+    assert.strictEqual(roman(1), 'I');
+    assert.strictEqual(roman(5), 'V');
+    assert.strictEqual(roman(1000), 'M');
+    assert.strictEqual(roman(50), 'L');
+    assert.strictEqual(roman(500), 'D');
 
-		assert.strictEqual(roman(4), 'IV');
-		assert.strictEqual(roman(900), 'CM');
+    assert.strictEqual(roman(4), 'IV');
+    assert.strictEqual(roman(900), 'CM');
 
-		assert.strictEqual(roman(1904), 'MCMIV');
-		assert.strictEqual(roman(1990), 'MCMXC');
-		assert.strictEqual(roman(2017), 'MMXVII');
-	});
+    assert.strictEqual(roman(1904), 'MCMIV');
+    assert.strictEqual(roman(1990), 'MCMXC');
+    assert.strictEqual(roman(2017), 'MMXVII');
+  });
 
-	QUnit.test('roman правильно определяет, что было передано на вход', function (assert) {
-		assert.strictEqual(roman('1904'), 'MCMIV');
-		assert.strictEqual(roman('1990'), 'MCMXC');
-		assert.strictEqual(roman('2017'), 'MMXVII');
-	});
+  QUnit.test('roman правильно определяет, что было передано на вход', function (assert) {
+    assert.strictEqual(roman('1904'), 'MCMIV');
+    assert.strictEqual(roman('1990'), 'MCMXC');
+    assert.strictEqual(roman('2017'), 'MMXVII');
+  });
 
-	QUnit.test('roman правильно обрабатывает некорректные аргументы', function (assert) {
-		assert.throws(() => roman(-25), TypeError('Roman numerals cannot be negative.'));
-		assert.throws(() => roman(Infinity), TypeError('Number is too big.'));
-		assert.throws(() => roman(10000), TypeError('Number is too big.'));
-		let error = new TypeError('Incorrect argument.');
+  QUnit.test('roman правильно обрабатывает некорректные аргументы', function (assert) {
+    assert.throws(() => roman(-25), TypeError('Roman numerals cannot be negative.'));
+    assert.throws(() => roman(Infinity), TypeError('Number is too big.'));
+    assert.throws(() => roman(10000), TypeError('Number is too big.'));
+    let error = new TypeError('Incorrect argument.');
         let argsSet = [
-			['12asd'],
-			['LVIIO'],
-			[NaN],
-			[undefined]
+          ['12asd'],
+          ['LVIIO'],
+          [NaN],
+          [undefined]
         ];
         for (let args of argsSet) {
-            assert.throws(() => roman(...args), error);
-		}
-	});
+        assert.throws(() => roman(...args), error);
+    }
+  });
 });
